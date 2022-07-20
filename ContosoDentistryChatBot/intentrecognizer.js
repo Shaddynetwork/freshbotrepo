e03bc5a28a239c017f9d170d3b0f6ed620661c8c
Shaddynetwork <71704518+Shaddynetwork@users.noreply.github.com>
2022-07-20 20:02:10 +0000

    updating dentistscheduler

--- ContosoDentistryChatBot/intentrecognizer.js
+++ ContosoDentistryChatBot/intentrecognizer.js
@@ -31,10 +31,10 @@ class IntentRecognizer {
         const datetimeEntity = result.entities.datetime;
         if (!datetimeEntity || !datetimeEntity[0]) return undefined;
 
-        const timex = datetimeEntity[0].timex;
+       const timex = datetimeEntity[0].timex;
         if (!timex || !timex[0]) return undefined;
 
-        const datetime = timex[0]
+        const datetime = result.entitites//timex[0]
         return datetime;
     }
 }
