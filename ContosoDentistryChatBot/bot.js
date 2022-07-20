91ed4300ac600bd456e899f561ee6b4fb5b87ca5
Shaddynetwork <71704518+Shaddynetwork@users.noreply.github.com>
2022-07-20 19:35:28 +0000

    updated bot.js

--- ContosoDentistryChatBot/bot.js
+++ ContosoDentistryChatBot/bot.js
@@ -14,20 +14,22 @@ class DentaBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker()
+        this.QnAMaker = new QnAMaker(configuration.QnAConfiguration)
        
         // create a DentistScheduler connector
-      
+        this.DentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration);
         // create a IntentRecognizer connector
-
+        this.IntentRecognizer = new IntentRecognizer(configuration.LuisConfiguration);
 
         this.onMessage(async (context, next) => {
+            try{
             // send user input to QnA Maker and collect the response in a variable
             // don't forget to use the 'await' keyword
-          
+            const qnaResults = await this.QnAMaker.getAnswers(context);
             // send user input to IntentRecognizer and collect the response in a variable
             // don't forget 'await'
-                     
+            const LuisResult = await this.IntentRecognizer .executeLuisQuery(context); 
+            const topIntent = result.luisResult.prediction.topIntent;       
             // determine which service to respond with based on the results from LUIS //
 
             // if(top intent is intentA and confidence greater than 50){
@@ -37,17 +39,31 @@ class DentaBot extends ActivityHandler {
             //  return;
             // }
             // else {...}
-             
+            let message;
+            if (result.intents[topIntent].score>0.65) {
+                if (topIntent === 'getAvailability') {
+                    message = await this.DentistScheduler.getAvailability(this.IntentRecognizer.getTimeEntity(result));
+                } else {
+                    message = await this.DentistScheduler.scheduleAppointment(this.IntentRecognizer.getTimeEntity(result));
+                };
+            } else {
+                message = answers[0].answer;
+            }
+                // If no answers were returned from QnA Maker, reply with help.
+            await context.sendActivity(MessageFactory.text(message, message));
+        } catch(e) {
+            console.error(e);
+        } 
             await next();
     });
 
         this.onMembersAdded(async (context, next) => {
         const membersAdded = context.activity.membersAdded;
         //write a custom greeting
-        const welcomeText = '';
+        const greetingText = '';
         for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
             if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                await context.sendActivity(MessageFactory.text(greetingText, greetingText));
             }
         }
         // by calling next() you ensure that the next BotHandler is run.
